f7329762343e400c1b6149423e7533b32303f737
Emmanuel <emmanuel@GwU777.(none)>
2010-04-05 21:24:12 +0200

    commit 5

--- test.php
+++ test.php
@@ -5,5 +5,6 @@ echo "Flipping long to test git! - commit 2";
 echo "Commit 2 didn't work - commit 3";
 
 echo "I have to had the flipping file again and again, why o why...";
-
+echo "another commit to check the add option";
+echo "I have to add the fliping file every single time, crap...";
 ?>
