5fdb8404e38a5e40341d2dd642ceea0cf303a43e
Emmanuel <emmanuel@GwU777.(none)>
2010-04-05 21:27:04 +0200

    Commit 8 after modif

--- test.php
+++ test.php
@@ -6,5 +6,6 @@ echo "Commit 2 didn't work - commit 3";
 
 echo "I have to had the flipping file again and again, why o why...";
 echo "another commit to check the add option";
-echo "I have to add the fliping file every single time, crap...";
+echo "I have to add the fliping file every single time, should not speak badly - ,modified on this commit...";
+echo "I have to first press ctrl X, then o, then enter before commiting, flipping long";
 ?>
