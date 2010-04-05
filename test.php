207c83dfe788025b06d4e149a7b3dc5db6374897
Emmanuel <emmanuel@GwU777.(none)>
2010-04-05 21:19:24 +0200

    commit 4 with -a

--- test.php
+++ test.php
@@ -4,4 +4,6 @@ echo "I am testing git - commit 1";
 echo "Flipping long to test git! - commit 2";
 echo "Commit 2 didn't work - commit 3";
 
+echo "I have to had the flipping file again and again, why o why...";
+
 ?>
