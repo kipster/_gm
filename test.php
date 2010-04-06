1824da96ed7189f9fead801872ccb31d74970aef
Emmanuel <emmanuel@GwU777.(none)>
2010-04-06 17:10:08 +0200

    Quick Commit

--- test.php
+++ test.php
@@ -8,4 +8,5 @@ echo "I have to had the flipping file again and again, why o why...";
 echo "another commit to check the add option";
 echo "I have to add the fliping file every single time, should not speak badly - ,modified on this commit...";
 echo "I have to first press ctrl X, then o, then enter before commiting, flipping long";
+echo "last commit¨;
 ?>
